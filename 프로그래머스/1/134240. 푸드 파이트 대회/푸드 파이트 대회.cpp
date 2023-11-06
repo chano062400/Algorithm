f5f8095fa3414fdb87daedc35897b9531ba185a2
chano062400 <144113194+chano062400@users.noreply.github.com>
2023-11-06 18:54:17 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -5,14 +5,7 @@ using namespace std;
 
 string solution(vector<int> food) {
     string answer = "";
-    
-    int sum =0;
-    for(int i=1; i<food.size(); i++)
-    {
-        if(food[i] % 2 == 1) food[i]--;
-        sum += food[i];
-    }
-    
+
     string str = "";
     int index =1, cnt =0;
     while(index < food.size())
