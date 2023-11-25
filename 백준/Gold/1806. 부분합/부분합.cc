0f8bb69f3b2119e3f95316a11af4e332e8546e0c
chano062400 <144113194+chano062400@users.noreply.github.com>
2023-11-25 15:10:21 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int arr[100000];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, s;
+	cin >> n >> s;
+	int left = 0, high = 0;
+	for (int i = 0; i < n; i++) 
+		cin >> arr[i];
+	int sum = arr[0];//5
+	int res = 1000000001;
+	while (left <= high && high < n) {
+		if (sum < s) {
+			sum += arr[++high];
+		}
+		else if (sum >= s) {
+			res = min(res, high - left + 1);
+			sum -= arr[left++];
+		}
+	}
+	if (res == 1000000001) {
+		cout << 0 << endl;
+	}
+	else cout << res << endl;
+	return 0;
+}
