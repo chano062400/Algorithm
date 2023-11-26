6601ccd5e933934d6779fe4ed2350e0079feb50c
chano062400 <144113194+chano062400@users.noreply.github.com>
2023-11-26 17:34:14 +0900

    [Silver II] Title: 안녕, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <memory.h>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <regex>
+#include <map>
+#include <cstdlib>
+#include <list>
+using namespace std;
+
+int hp[20], joy[20], dp[101], n, res = 0;
+
+void dfs(int curhp, int curjoy, int idx)
+{
+    if (curhp >= 100 || idx > n) return;
+    
+	res = max(res, curjoy);
+
+    dfs(curhp + hp[idx], curjoy + joy[idx], idx + 1);
+    dfs(curhp, curjoy, idx + 1);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> hp[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> joy[i];
+    }
+    
+    dfs(0, 0, 0);
+    
+    cout << res;
+}
