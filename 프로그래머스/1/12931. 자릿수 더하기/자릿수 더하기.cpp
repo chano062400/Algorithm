7e498c321d02c27c8843a3b98b388702a85fc811
chano062400 <144113194+chano062400@users.noreply.github.com>
2023-12-05 23:27:19 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    string str = to_string(n);
+    for(auto s : str) answer += s - '0';
+    return answer;
+}
