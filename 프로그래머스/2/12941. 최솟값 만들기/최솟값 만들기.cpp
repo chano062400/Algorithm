9cea6d931b713aaf70c6efcca0ada848f188d8f8
chano062400 <144113194+chano062400@users.noreply.github.com>
2023-12-20 17:14:46 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.08 ms, Memory: 3.79 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+    sort(A.begin(), A.end(), greater<int>());
+    sort(B.begin(), B.end());
+
+    while(!A.empty())
+    {
+        answer += A.back() * B.back();
+        A.pop_back();
+        B.pop_back();
+    }
+    
+    return answer;
+}
