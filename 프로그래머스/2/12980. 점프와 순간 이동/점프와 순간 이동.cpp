a48e8c568d5af95234729dc6a605ee4b18ab5aa2
chano062400 <144113194+chano062400@users.noreply.github.com>
2023-12-27 19:14:01 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.85 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+    while(n > 0)
+    {
+        if(n % 2 == 1 ) 
+        {
+            n -= 1;
+            ans++;
+        }
+        else n /= 2;
+    }
+    
+    return ans;
+}
