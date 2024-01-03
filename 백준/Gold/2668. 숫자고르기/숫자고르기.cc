7a3f9a2076f221ce710ef547f47e49be9ffc974c
chano062400 <144113194+chano062400@users.noreply.github.com>
2024-01-03 16:48:53 +0900

    [Gold V] Title: 숫자고르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.cc
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.cc
@@ -17,41 +17,19 @@ using namespace std;
 
 int n, arr[3][101];
 bool visited[101];
-vector<int> v, one ,two;
+vector<int> v;
 
-void dfs(int cur)
+void dfs(int cur, int start)
 {
-    if (!visited[cur])
+    if (visited[cur]) 
     {
-        visited[cur] = 1;
-        one.push_back(cur);
-        two.push_back(arr[2][cur]);
-    }
-    else
-    {
-        sort(one.begin(), one.end());
-        sort(two.begin(), two.end());
-
-        bool same = true;
-
-        for (int i = 0; i < one.size(); i++)
-        {
-            if (one[i] != two[i])
-            {
-                same = false;
-                break;
-            }
-        }
-
-        if (same)
-        {
-             for (auto n : one) v.push_back(n);
-        }
-
+        if (cur == start) // 사이클 발생
+            v.push_back(cur);
         return;
-    }    
-
-    dfs(arr[2][cur]);
+    }
+    
+    visited[cur] = 1;
+    dfs(arr[2][cur], start);
 
     return;
 }
@@ -74,20 +52,11 @@ int main()
 
     for (int i = 1; i <= n; i++)
     {
-        memset(visited, 0, sizeof(visited));
-        one.clear();
-        two.clear();
-        dfs(i);
-    }
-
-    // 1번쨰 줄과 2번째 줄 값이 같은 애들.
-    for (int i = 1; i <= n; i++)
-    {
-        if (find(v.begin(), v.end(), i) == v.end() && arr[2][i] == i)  v.push_back(i);
+		memset(visited, 0, sizeof(visited));
+		dfs(i,i);
     }
 
     sort(v.begin(), v.end());
-    v.erase(unique(v.begin(), v.end()), v.end());
     cout << v.size() << '\n';
     for (auto n : v) cout << n << '\n';
 
