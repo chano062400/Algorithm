5620d95457089c93c80a1ca9f44689de85957f87
chano062400 <144113194+chano062400@users.noreply.github.com>
2024-04-27 20:33:33 +0900

    [Gold II] Title: 반도체 설계, Time: 4 ms, Memory: 2692 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <memory.h>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <regex>
+#include <map>
+#include <cstdlib>
+#include <list>
+#include <sstream>
+#include <bitset>
+#include <set>
+#include <unordered_map>
+#include <numeric>
+#include <memory>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n), lis;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    lis.push_back(v[0]);
+
+    for (int i = 1; i < n; i++)
+    {
+        if (lis.back() < v[i])
+        {
+            lis.push_back(v[i]);
+            continue;
+        }
+
+        auto it = lower_bound(lis.begin(), lis.end(), v[i]);
+        *it = v[i];
+    }
+
+    cout << lis.size() << '\n';
+
+}
+    
