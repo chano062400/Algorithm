98864976215fe1369a9ee88ac0dae7b45f41f164
chano062400 <144113194+chano062400@users.noreply.github.com>
2024-10-03 22:42:10 +0900

    [Silver II] Title: 생태학, Time: 256 ms, Memory: 2296 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <memory.h>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <regex>
+#include <map>
+#include <cstdlib>
+#include <list>
+#include <sstream>
+#include <bitset>
+#include <set>
+#include <unordered_map>
+#include <numeric>
+#include <memory>
+using namespace std;
+
+map<string, float> trees;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	float sum = 0.f;
+	while (1)
+	{
+		string str;
+		getline(cin, str);
+		
+		if (cin.eof() == true) break;
+		
+		if (trees.find(str) == trees.end())
+		{
+			trees.insert({ str,1.f });
+		}
+		else trees[str] += 1.f;
+		sum += 1.f;
+	}
+
+
+	for (const auto& tree : trees)
+	{
+		float divide = (tree.second / sum) * 100.f;
+		cout << fixed;
+		cout.precision(4);
+	
+		cout << tree.first << " " << divide << '\n';
+	}
+}
+
