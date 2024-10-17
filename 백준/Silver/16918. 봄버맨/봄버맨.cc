a4198b7ac55a2eca6b38b2d8e7d3933b8ba1e37e
chano062400 <144113194+chano062400@users.noreply.github.com>
2024-10-17 17:01:47 +0900

    [Silver I] Title: 봄버맨, Time: 32 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.cc
+++ 백준/Silver/16918. 봄버맨/봄버맨.cc
@@ -24,34 +24,29 @@ int install[201][201];
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
 int r, c, n, second = 2;
-queue<pair<int, int>> q;
 
 // 0 초기 1 무 2 설치 3 폭파 4 설치 5 폭파
 
 void explosion(int cursecond)
 {
-	int size = q.size();
-	for(int idx = 0; idx < size; idx++)
+	for (int i = 1; i <= r; i++)
 	{
-		int cx = q.front().first;
-		int cy = q.front().second;
-		q.pop();
-		if (cursecond - install[cx][cy] == 3)
+		for (int j = 1; j <= c; j++)
 		{
-			for (int i = 0; i < 4; i++)
+			if (arr[i][j] == 'O' && cursecond - install[i][j] == 3)
 			{
-				int nx = cx + dx[i];
-				int ny = cy + dy[i];
-				if (nx < 1 || nx > r || ny < 1 || ny > c) continue;
-				if (install[cx][cy] == install[nx][ny]) continue;
+				arr[i][j] = '.';
 
-				arr[nx][ny] = '.';
-				install[nx][ny] = -1;
+				for (int k = 0; k < 4; k++)
+				{
+					int nx = i + dx[k];
+					int ny = j + dy[k];
+					if (nx < 1 || nx > r || ny < 1 || ny > c) continue;
+					if (install[nx][ny] == install[i][j]) continue;
+					arr[nx][ny] = '.';
+				}
 			}
-			arr[cx][cy] = '.';
-			install[cx][cy] = -1;
 		}
-		else q.push({ cx,cy });
 	}
 }
 
@@ -65,7 +60,6 @@ void bombinstall(int cursecond)
 			{
 				arr[i][j] = 'O';
 				install[i][j] = cursecond;
-				q.push({i,j});
 			}
 		}
 	}
@@ -87,7 +81,6 @@ int main()
 			if (arr[i][j] == 'O')
 			{
 				install[i][j] = 0;
-				q.push({i,j});
 			}
 			else
 			{
@@ -96,19 +89,19 @@ int main()
 		}
 	}
 
-	if (second < 1)
-	{
-		for (int i = 1; i <= r; i++)
-		{
-			for (int j = 1; j <= c; j++)
-			{
-				cout << arr[i][j];
-			}
-			cout << '\n';
-		}
-		return 0;
-	}
-
+    if(n < 1)
+    {
+        for (int i = 1; i <= r; i++)
+	    {
+		    for (int j = 1; j <= c; j++)
+		    {
+			    cout << arr[i][j];
+		    }
+		cout << '\n';
+	    }
+        return 0;
+    }
+    
 	while (second < n + 1)
 	{
 		if (second % 2 == 0)
