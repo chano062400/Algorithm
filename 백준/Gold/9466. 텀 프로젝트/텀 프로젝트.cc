56da3c4262c6ba49bfcd0483a8b153826f73321c
chano062400 <144113194+chano062400@users.noreply.github.com>
2024-11-01 15:37:04 +0900

    [Gold III] Title: 텀 프로젝트, Time: 492 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -1,56 +1,59 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 #include <cstring>
-#define MAX 100001
 using namespace std;
 
-int t, n;
-int graph[MAX];
-bool visited[MAX];
-bool done[MAX];
+int visited[100001]; // 0: 미방문, 1: 방문 중, 2: 방문 완료
 int cnt;
-void hasCycle( int node) {
 
-	visited[node] = true;
-	int next = graph[node];
+void dfs(int cur, vector<int>& v)
+{
+    visited[cur] = 1; // 방문 중
+    int next = v[cur];
 
-	
-	if (!visited[next]) {
-		hasCycle( next);
-	}
-	else if (!done[next]) {//방문은 했지만 아직 사이클이 아니라면 next까지 포함해서 사이클 완성
-		//자기 자신을 포함한 팀의 멤버를 카운트
-		for (int i = next; i != node; i = graph[i]) {
-			cnt++;
-		}
-		cnt++;
-	}
-	done[node] = true;
+    if (visited[next] == 0) // 미방문 노드
+    {
+        dfs(next, v);
+    }
+    else if (visited[next] == 1) // 사이클 발견
+    {
+        cnt++;
+        for (int i = next; i != cur; i = v[i])
+            cnt++;
+    }
+    // 방문 완료 표시
+    visited[cur] = 2;
 }
 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n + 1);
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> v[i];
+        }
 
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	cin >> t;
-	while (t--) {
-		cin >> n;
-		for (int i = 1; i <= n; i++) {
-			cin >> graph[i];
-		}
-		for (int i = 1; i <= n; i++) {
-			if (!visited[i]) {
-				hasCycle(i);
-			}
-		}
-		cout << n-cnt << '\n';
-		cnt = 0;
-		memset(visited, false, sizeof(visited));
-		memset(done, false, sizeof(done));
-	}
+        memset(visited, 0, sizeof(visited));
+        cnt = 0;
 
+        for (int i = 1; i <= n; i++)
+        {
+            if (visited[i] == 0)
+            {
+                dfs(i, v);
+            }
+        }
 
-	return 0;
+        cout << n - cnt << '\n';
+    }
 }
