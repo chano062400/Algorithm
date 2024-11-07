e79a44baf0e1cc50fdacbf97d662a2cd76d6c238
chano062400 <144113194+chano062400@users.noreply.github.com>
2024-11-07 19:39:31 +0900

    [level 2] Title: 테이블 해시 함수, Time: 13.31 ms, Memory: 57.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool cmp(vector<int> v1, vector<int> v2, int col)
+{
+    if(v1[col - 1] == v2[col - 1]) return v1[0] > v2[0];
+    return v1[col - 1] < v2[col - 1];
+}
+
+int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
+    int answer = 0;
+    bitset<23> ans(answer);
+    sort(data.begin(), data.end(),
+         [col] (vector<int> v1, vector<int> v2) 
+         {
+             return cmp(v1,v2,col);
+         }
+    );
+    
+    for(int i = row_begin - 1; i <= row_end - 1; i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < data[i].size(); j++)
+        {
+            sum += data[i][j] % (i + 1);
+        }
+        bitset<23> bs(sum);
+        ans ^= bs;
+    }
+
+    return ans.to_ulong();
+}
