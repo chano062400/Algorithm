7939957803738be91efc522cfb5ff506b4a91aea
chano062400 <144113194+chano062400@users.noreply.github.com>
2024-11-26 18:18:06 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -2,49 +2,34 @@
 
 using namespace std;
 
-int getfastestcursor(int size, int prev, int cur)
-{
-    int left, right;
-    if(prev < cur)
-    {    
-        right = cur - prev;
-        left = prev + 1 + (size - cur);
-    }
-    else
-    {
-        right = size - prev + cur + 1;
-        left = prev - cur;
-    }
-    return min(right, left);
-}
-
-int getfastestalpha(char target)
-{
-    int up = target - 'A';
-    int down = 1 + 'Z' - target;
-    return min(up, down);
-}
-
 int solution(string name) {
-    int answer = 987654321;
-    vector<int> v;
-    for(int i = 0; i < name.length(); i++)
-    {
-        if(name[i] == 'A') continue;
-        v.push_back(i);
-    }
-    
-    do
-    {
-        int temp = 0, prev = 0;
-        for(int i = 0; i < v.size(); i++)
-        {
-            temp += getfastestcursor(name.length() - 1, prev, v[i]);
-            prev = v[i];
-            temp += getfastestalpha(name[v[i]]);
-        }
-        answer = min(answer, temp);
-    }while(next_permutation(v.begin(),v.end()));
+    int answer = 0, n = name.length();
 
+    // 알파벳 변경 최소 이동 계산
+    for (char c : name) 
+    {
+        answer += min(c - 'A', 'Z' - c + 1);
+    }
+
+    if(answer == 0) return 0;
+    
+    // 좌우 이동 최소값 계산
+    int min_moves = n - 1; // 기본적으로 오른쪽 끝까지 이동하는 경우
+    for (int i = 0; i < n; i++) {
+        if (name[i] == 'A') continue;
+        int next = i + 1;
+        while (next < n && name[next] == 'A') 
+        {
+            ++next;
+        }
+        
+        // 1. 오른쪽으로 i를 먼저 탐색 후, 왼쪽으로 조작해서 next를 탐색 
+        int case1 = i * 2 + (n - next);
+        // 2. 왼쪽으로 next를 먼저 탐색 후, 오른쪽으로 조작해서 i를 탐색
+		int case2 = i + (n - next) * 2;
+        min_moves = min(min_moves, min(case1, case2));
+    }
+
+    answer += min_moves;
     return answer;
 }
