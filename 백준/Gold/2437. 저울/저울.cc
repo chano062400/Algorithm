012733dd7c533633e1bf5f8aa44bbc0ae7995986
chano062400 <144113194+chano062400@users.noreply.github.com>
2025-01-28 16:41:57 +0900

    [Gold II] Title: 저울, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include "memory.h"
+#include <set>
+#include <unordered_set>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <list>
+#include <numeric>
+#include <limits.h>
+#include <list>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, res = 1;
+    cin >> n;
+    
+    vector<int> v(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        int num = v[i];
+        if (num > res) break;
+        res += num;
+    }
+
+    cout << res;
+}
