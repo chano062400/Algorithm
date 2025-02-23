1129164d2f01b95e0a55b59e2ca7a7ddea48f2c0
chano062400 <144113194+chano062400@users.noreply.github.com>
2025-02-23 22:07:40 +0900

    [Silver I] Title: 접두사, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1141. 접두사/접두사.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include "memory.h"
+#include <set>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <list>
+#include <numeric>
+#include <limits.h>
+#include <list>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    
+    vector<string> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+    int res = n;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        string next = v[i + 1].substr(0, v[i].size());
+        if (next == v[i])
+        {
+            res--;
+        }
+    }
+
+    cout << res;
+}
